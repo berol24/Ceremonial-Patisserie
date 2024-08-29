32551aeb17c8cb5cfaadd4d77a6a51af44f9de09
bertino2 <berolbertindjomo@gmail.com>
2024-08-29 14:46:12 +0200

    finalisation de la page de detail commande

--- src/components/CakeDetail/CakeDetail.js
+++ src/components/CakeDetail/CakeDetail.js
@@ -56,12 +56,13 @@ function CakeDetail() {
   return (
     <div>
       <Header />
-
+      <a href="tel:+33610810730" target="_blank"  rel="noopener noreferrer">
+        <img src="#" alt="Téléphone" class="phone-icon" />
+      </a>
       <h1 className="title_detail_commande">Détails de ma commande</h1>
       <div className="detail_commande" key={cake.id}>
         <div className="left_detail_commande">
           <img src={`/assets/images/${cake.url_image}.png`} alt={cake.name} />
-
         </div>
         <div className="right_detail_commande">
           <div className="name_price">
@@ -70,7 +71,7 @@ function CakeDetail() {
           </div>
 
           <div className="description_cake">
-            <p>{cake.description}lorem lorem ipdsdsdsdslorem ipdsdsdsdslorem ipdsdsdsdslorem ipdsdsdsdslorem ipdsdsdsdslorem ipdsdsdsdslorem ipdsdsdsds</p>
+            <p>{cake.description}lorem lorem ipdsdsdsdsloremipdsdsdsdsloremipdsdsdsdslipdipdsdsdsdsloremipdsdsdsdsloremipdsdsdsdsloremipdsdsdsdsloremipdsdsdsdsloremipdsdsdsdsloremipdsdsdsdsipdsdsdsdsloremipdsdsdsdsloremipdsdsdsdsloremipdsdsdsdsloremipdsdsdsdsloremipdsdsdsdsloremipdsdsdsdssdsdsdsloremipdsdsdsdsloremipdsdsdsdsloremipdsdsdsdsloremipdsdsdsdsloremipdsdsdsdsloremipdsdsdsdsoremipdsdsdsdsloremipdsdsdsdsloremipdsdsdsdsloremipdsdsdsds</p>
           </div>
 
           <div  className="btn_add_remove">
